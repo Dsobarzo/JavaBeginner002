203a01f616f0110dba57d6a829fa964d8e77a2f8
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-10 18:00:04 -0300

    Se crea clase whiledowhile
    Se cre clase whiledowhile y se inicializa, se muestra por consola

---
+++ PrimerProyecto/WhileDoWhile.java
@@ -0,0 +1,16 @@
+public class WhileDoWhile
+{
+     public static void main(String[] args){
+                                //0   1   2   3   4
+        int [] arregloEnteros = {100,200,300,400,500};
+        String [] arregloTextos = {"uno","dos","tres","cuatro","cinco"};
+        int i = 0;
+        //Mientras
+        while(i <= 4){
+            System.out.println(arregloEnteros[i]);
+            i++;
+        }
+    
+    }
+}
+
