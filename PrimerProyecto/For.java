6aee1c141b98b89667e8fe7ba3cdb62055c93796
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-10 17:53:20 -0300

    Se creo Clase for
    Se creo e inplemeto un for para recorrer el arreglo

---
+++ PrimerProyecto/For.java
@@ -0,0 +1,18 @@
+public class For
+{
+    public static void main(String[] args){
+                                //0   1   2   3   4
+        int [] arregloEnteros = {100,200,300,400,500};
+        String [] arregloTextos = {"uno","dos","tres","cuatro","cinco"};
+        
+        for(int i = 0; i<= 4 ;i++){
+            System.out.println(arregloEnteros[i]);
+        }
+        for(int i = 0; i<= 4 ;i++){
+            System.out.println(arregloTextos[i]);
+        }
+        
+        
+    
+    }
+}
