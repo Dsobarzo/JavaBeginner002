634e75a9488881653507b5c931dad10366aa8848
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-10 15:57:54 -0300

    Mod variable
    Se generouna variable y luego se modifico en las lineas de codigo

--- PrimerProyecto/PrimerClase.java
+++ PrimerProyecto/PrimerClase.java
@@ -3,7 +3,12 @@ public class PrimerClase
     //Metodo Main
     public static void main(String[] args)
     {
-        System.out.println("Hola Mundo");
+        //declaracion de Variable
+        int numeroEntero = 18;
+        System.out.println("Hola Mundo, mi edad es: " + numeroEntero);
+        //Cambio de valor de la Variable
+        numeroEntero = 26;
+        System.out.println("Hola Mundo, mi edad es: " + numeroEntero);
     }
     
 }
