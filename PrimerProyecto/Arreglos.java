7a27072b2fe1b55e94a502e65b06b924ed2cc8d4
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-10 17:42:35 -0300

    inicializa arreglos
    Se inicializan arreglos y se muestran por pantalla

--- PrimerProyecto/Arreglos.java
+++ PrimerProyecto/Arreglos.java
@@ -1,11 +1,12 @@
 public class Arreglos
 {
     public static void main(String[] args){
+                                //0   1   2   3   4
+        int [] arregloEnteros = {100,200,300,400,500};
+        String [] arregloTextos = {"uno","dos","tres","cuatro","cinco"};
+        
+        System.out.println(arregloEnteros[0]);
+        System.out.println(arregloTextos[2]);
     
-        int mesNumero = 12;
-        String mesTexto = "";
-        
-        System.out.printls(mesTexto);
-        
     }
 }
