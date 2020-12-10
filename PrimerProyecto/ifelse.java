7551ed379197b0153f8d5b59ef14a578b9de5d84
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-10 16:51:55 -0300

    Se crea IF
    Se creo una sentencia de decicion If Else

---
+++ PrimerProyecto/ifelse.java
@@ -0,0 +1,38 @@
+public class ifelse
+{
+    public static void main (String[] args){
+     
+      int edad = 19;
+      String descripcionEdad = "";
+      String nombre = "Juan";
+      // if = si "la pregunta tiene respuesta verdadera" 
+      //entra a mi estructura
+      // < , > , == , >= , <= Operadores comparación
+      //Operadores Logicos
+      // && El operador L. "Y" y entra al ser todo verdadero
+      // || El operador L. "O" y entra al ser una o varias de las P.V.
+      if  ((edad >= 18 && edad <=40)
+      {
+        if (edad < 18 && nombre == Macarena)
+        {
+            
+            if (edad < 18 && nombre == Macarena)
+            {
+            
+            
+            }
+            }
+          
+      descripcionEdad = "Tu eres mayor de edad, si puedes entrar al bar";   
+     System.out.println(descripcionEdad);
+        
+        }else if (nombre == "Miranda"){
+        descripcionEdad = "Tu eres menor de edad, no puedes pasar al bar";
+         System.out.println(descripcionEdad);
+        
+        }
+    
+      
+    } 
+    
+}
