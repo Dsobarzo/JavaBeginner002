5b6f6074e28295f1f8175af5eb9ec1504bddc51c
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-10 17:26:18 -0300

    Se crear clase arreglos

---
+++ PrimerProyecto/Arreglos.java
@@ -0,0 +1,11 @@
+public class Arreglos
+{
+    public static void main(String[] args){
+    
+        int mesNumero = 12;
+        String mesTexto = "";
+        
+        System.out.printls(mesTexto);
+        
+    }
+}
