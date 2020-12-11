5016c324b227d1d086e18a553557a5c4679188a0
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-11 15:58:16 -0300

    Ejmplo de poo con la clase automovil

---
+++ PrimerProyecto/Automovil.java
@@ -0,0 +1,23 @@
+public class Automovil
+{
+    // variables de instancia caracteristicas o propiedadea de una clase automovil
+    private int x;
+
+    /**
+     * el contructor sirve para inicializar las variable de instancia
+     */
+    public Automovil()
+    {
+        // se inicualiza la variable de instancia con 0
+        x = 0;
+    }
+
+    /**
+     * los metodos son funcionalidades de la clase (Ej acelerar, frenar.. etc)
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ PrimerProyecto/Metodos2.java
@@ -0,0 +1,9 @@
+public class Metodos2
+{
+      public static void main (String[] args){
+        
+       
+        
+    }
+  
+}
